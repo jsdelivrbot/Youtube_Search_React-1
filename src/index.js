5c0fcaaad4e87a97d22e4cb75edf31544d7d9e57
Kristopher Parrillo <kkparrillo@gmail.com>
2017-05-08 17:21:33 -0700

    adds YTSearch get request

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+const API_KEY = 'AIzaSyC-mL3U0mJd9zMUvI0th7igveOzxbyyoFU';
 const container = document.querySelector('.container');
 
 
-const API_KEY = 'AIzaSyC-mL3U0mJd9zMUvI0th7igveOzxbyyoFU';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            videos: []
+        };
 
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({ videos })
+        });
+     }
 
-const App = () => {
-    return (
-        <div>   
-            <SearchBar />
-        </div>
-    );
-};
+    render() {
+        return (
+            <div>   
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, container);
